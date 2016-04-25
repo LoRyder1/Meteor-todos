3bf51eb5e11414e85c584d0defe73f690ef3932e
Lovinder Pnag <lspnag@gmail.com>
2016-04-25 06:47:00 -0400

    extend react and clear the form

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,6 +8,21 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+  handleSubmit(event) {
+    event.prevenDefault();
+
+    // Find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
@@ -44,6 +59,6 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}), {sort: {createdAt: -1 } }).fetch(),
   };
 }, App);
