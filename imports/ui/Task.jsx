60a5a285d479d669e8b0eba2422272ab3a5b53df
Lovinder Pnag <lspnag@gmail.com>
2016-04-27 16:55:05 -0400

    storing temporary data

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -14,10 +14,27 @@ export default class Task extends Component {
   deleteThisTask() {
     Tasks.remove(this.props.task._id);
   }
-  
+
   render() {
+    // Give tasks a different className when they are checked off, 
+    // so that we can style them nicely in CSS
+    const taskClassName = this.props.task.checked ? 'checked' : '';
+
     return (
-      <li>{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
+
+        <input
+          type='checkbox'
+          readOnly
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        <span className="text">{this.props.task.text}</span>
+      </li>
     );
   }
 }
