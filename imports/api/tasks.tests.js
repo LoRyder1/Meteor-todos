c193b6d17d38d65b2c0c7c64f123faf94cb0949e
Lovinder Pnag <lspnag@gmail.com>
2016-04-28 07:58:43 -0400

    add scaffold for testing

---
+++ imports/api/tasks.tests.js
@@ -0,0 +1,12 @@
+// eslint-env mocha
+
+import { Meteor } from 'meteor/meteor';
+
+if (Meteor.isServer) {
+  describe('Tasks', () => {
+    desribe('methods', () => {
+      it('can delete owned task', () => {
+      });
+    });
+  });
+}
